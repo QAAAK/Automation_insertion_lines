35a37d595173e5ce901744af1d301511b57154db
QAAAK <dmitsantalov@yandex.ru>
2023-06-22 10:30:05 +0300

    Hello! Thanks you for watching my project!

---
+++ Automatic insertion of lines from the clipboard.py
@@ -0,0 +1,17 @@
+import pyperclip
+import time
+
+
+buffer_pause = ''
+
+while True:
+    buffer = pyperclip.paste()
+
+    if buffer != buffer_pause:
+        print(buffer)
+        with open('text.txt', 'a', encoding='utf-8') as file:
+            file.write(buffer)
+
+        buffer_pause = buffer
+    time.sleep(1)
+
